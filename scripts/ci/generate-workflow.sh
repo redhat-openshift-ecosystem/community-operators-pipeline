cdd16bcb1dd7bce445f21b16f7c8db9c573d93ff
Test User <test@example.com>
2021-06-25 14:39:53 +0200

    rm

--- scripts/ci/generate-workflow.sh
+++ scripts/ci/generate-workflow.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 cd /tmp
+rm -rf cpmmunity-operators
 git clone https://github.com/operator-framework/community-operators.git
 ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U https://github.com/operator-framework/operator-test-playbooks -C master -i localhost, set-pipeline-workflow.yml -e workflow_templates_path=/tmp/community-operators/scripts/template/workflow/ -e workflow_config_path=./ci -vv
 
