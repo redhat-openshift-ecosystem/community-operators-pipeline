99efe77a9ee4bb5b62c25bcacdbeaa6d1f0b9c54
Test User <test@example.com>
2021-06-25 14:54:39 +0200

    location

--- scripts/ci/generate-workflow.sh
+++ scripts/ci/generate-workflow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-cd /tmp
-rm -rf cpmmunity-operators
-git clone https://github.com/operator-framework/community-operators.git
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U https://github.com/operator-framework/operator-test-playbooks -C master -i localhost, set-pipeline-workflow.yml -e workflow_templates_path=/tmp/community-operators/scripts/template/workflow/ -e workflow_config_path=./ci -vv
+#rm -rf /tmp/community-operators
+#git clone https://github.com/operator-framework/community-operators.git /tmp/community-operators
+BASE_LOCATION=$(pwd)
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U https://github.com/operator-framework/operator-test-playbooks -C master -i localhost, upstream/set-pipeline-workflow.yml -e ansible_connection=local -e workflow_templates_path=/tmp/community-operators/scripts/template/workflow/ -e workflow_config_path=$BASE_LOCATION -vv
 
