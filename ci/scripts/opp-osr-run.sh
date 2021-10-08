c51e462420d4439f7af93d340d84949dd87ad8d4
Martin Vala <73172904+mvalarh@users.noreply.github.com>
2021-10-08 09:01:53 +0300

    Ci/dev (#134)
    * auto label
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * osr run type ocp
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Improved ANSIBLE_DISPLAY_SKIPPED_HOSTS
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * operator info from dev
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * debug operator dir
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * operator_info source change
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * remove debug
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * remove debug
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * dev to test operator_info
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * debug
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * artefacts upload/download
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * list
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * minus
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * load atef to lemon openshift
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * info path
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * parametrize artefact path and add to orange
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * parametrize 2
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * kiwi with operator_info
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * variable
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * variable2
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * test
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * test
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * test
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * test
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * hardcoded
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * mount downloaded artifacts
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * fix
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * file
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * production_registry_namespace added
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Artifacts in release (#133)
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * Artifacts in release
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * Artifacts in release
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * defaults
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * clone
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * clone
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * Fix opc
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Colloring
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Improved file operator base dir
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Set stable version
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    Co-authored-by: J0zi <jbreza@redhat.com>

--- ci/scripts/opp-osr-run.sh
+++ ci/scripts/opp-osr-run.sh
@@ -66,6 +66,7 @@ $CONTAINER_TOOL run -d --net=host --privileged -e STORAGE_DRIVER=vfs --rm -t --n
 $CONTAINER_TOOL exec -t \
 -e automatic_cluster_version_label=true \
 -e bundle_force_rebuild=true \
+-e cluster_type=ocp \
 -e OP_STREAM="$STREAM_NAME" \
 -e OP_NAME="$OP_NAME" \
 -e OP_VERSION="$OP_VER" \
