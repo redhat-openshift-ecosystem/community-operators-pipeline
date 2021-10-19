bcb8c54593a9f3b8006cc2fe519cbc0f348c013d
Martin Vala <mavala@redhat.com>
2021-10-19 20:49:53 +0300

    Test
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-update.sh
+++ ci/scripts/opp-update.sh
@@ -54,17 +54,16 @@ done
 yq --version
 MY_ANSIBLE_PULL_REPO=$(yq '.pipeline.playbooks.repo' $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml -r )
 MY_ANSIBLE_PULL_BRANCH=$(yq '.pipeline.playbooks.branch' $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml -r)
-
-#MY_ANSIBLE_PULL_REPO=$(cat $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml | yq '.pipeline.playbooks.repo')
-#MY_ANSIBLE_PULL_BRANCH=$(cat $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml | yq '.pipeline.playbooks.branch')
+echo "MY_ANSIBLE_PULL_REPO=$MY_ANSIBLE_PULL_REPO"
+echo "MY_ANSIBLE_PULL_BRANCH=$MY_ANSIBLE_PULL_BRANCH"
 
 for f in $OPP_FILES_TO_COPY_CI_SCRIPTS;do
     echo "Doing 'cp $OPP_TMP_DIR/opp-input/$OPP_CI_SCRIPTS_DIR/$f $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)'"
     cp $OPP_TMP_DIR/opp-input/$f $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
 
-    
-    sed -i 's/^PLAYBOOK_REPO=.*/PLAYBOOK_REPO=\"'$MY_ANSIBLE_PULL_REPO'\"/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
-    sed -i 's/^PLAYBOOK_REPO_BRANCH=.*/PLAYBOOK_REPO_BRANCH=\"'$MY_ANSIBLE_PULL_BRANCH'\"/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
+    cat $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
+    sed -i 's/^PLAYBOOK_REPO=.*/PLAYBOOK_REPO='$MY_ANSIBLE_PULL_REPO'/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
+    sed -i 's/^PLAYBOOK_REPO_BRANCH=.*/PLAYBOOK_REPO_BRANCH='$MY_ANSIBLE_PULL_BRANCH'/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
 done
 
 rm -rf $PWD/scripts
