729bfc676629b255f6108f95f3c54ddeb4d18d59
Martin Vala <73172904+mvalarh@users.noreply.github.com>
2021-10-19 21:37:03 +0300

    Ci/dev (#146)
    * Improved update
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Improved upgrade
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Improvments
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-update.sh
+++ ci/scripts/opp-update.sh
@@ -51,9 +51,23 @@ done
 
 [ -d $PWD/$OPP_CI_SCRIPTS_DIR ] || mkdir -p $PWD/$OPP_CI_SCRIPTS_DIR
 
+yq --version
+MY_ANSIBLE_PULL_REPO=$(yq '.pipeline.playbooks.repo' $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml -r )
+MY_ANSIBLE_PULL_BRANCH=$(yq '.pipeline.playbooks.branch' $PWD/ci/pipeline-config${CLUSTER_TYPE}.yaml -r)
+
+MY_ANSIBLE_PULL_REPO=${MY_ANSIBLE_PULL_REPO//\//\\\\/}
+MY_ANSIBLE_PULL_BRANCH=${MY_ANSIBLE_PULL_BRANCH//\//\\\\/}
+
+echo "MY_ANSIBLE_PULL_REPO=$MY_ANSIBLE_PULL_REPO"
+echo "MY_ANSIBLE_PULL_BRANCH=$MY_ANSIBLE_PULL_BRANCH"
+
 for f in $OPP_FILES_TO_COPY_CI_SCRIPTS;do
     echo "Doing 'cp $OPP_TMP_DIR/opp-input/$OPP_CI_SCRIPTS_DIR/$f $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)'"
     cp $OPP_TMP_DIR/opp-input/$f $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
+    echo "1"
+    sed -i 's/^PLAYBOOK_REPO=.*/PLAYBOOK_REPO='"$MY_ANSIBLE_PULL_REPO"'/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
+    echo "2"
+    sed -i 's/^PLAYBOOK_REPO_BRANCH=.*/PLAYBOOK_REPO_BRANCH='"$MY_ANSIBLE_PULL_BRANCH"'/g' $PWD/$OPP_CI_SCRIPTS_DIR/$(basename $f)
 done
 
 rm -rf $PWD/scripts
