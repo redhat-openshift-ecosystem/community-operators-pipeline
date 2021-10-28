2e6490c2667f88cb7ad3b43befecbda29a9823df
Martin Vala <mavala@redhat.com>
2021-10-28 10:03:37 +0300

    Improved message
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-env.sh
+++ ci/scripts/opp-env.sh
@@ -126,7 +126,7 @@ if [ -n "$OPP_REMOVED_FILES" ];then
     [[ $sf == scripts* ]] && OPP_CHANGES_CI=1 && continue
     [[ $sf == ci* ]] && OPP_CHANGES_CI=1 && continue
     [[ $sf == docs* ]] && OPP_CHANGES_DOCS=1 && continue
-    [[ $sf == operators* ]] ||  { echo "Not alloved changes outisde operators. Affected file : '$sf' !!! Exiting ..."; exit 1; }
+    [[ $sf == operators* ]] ||  { echo "Not alloved changes outside operators directory. Affected file : '$sf' !!! Exiting ..."; exit 1; }
     [[ $sf == *Dockerfile* ]] && OPP_CHANGES_DOCKERFILE=1 && continue
     [[ $sf == operators* ]] && OPP_CHANGES_IN_OPERATORS_DIR=1
     # [[ $sf == community-operators* ]] && OPP_CHANGES_IN_OPERATORS_DIR=1
@@ -212,7 +212,7 @@ for sf in ${OPP_ADDED_MODIFIED_FILES}; do
   [[ $sf == scripts* ]] && OPP_CHANGES_CI=1 && continue
   [[ $sf == ci* ]] && OPP_CHANGES_CI=1 && continue
   [[ $sf == docs* ]] && OPP_CHANGES_DOCS=1 && continue
-  [[ $sf == operators* ]] ||  { echo "Not alloved changes outisde operators. Affected file : '$sf' !!! Exiting ..."; exit 1; }
+  [[ $sf == operators* ]] ||  { echo "Not alloved changes outside operators directory. Affected file : '$sf' !!! Exiting ..."; exit 1; }
   [[ $sf == *Dockerfile* ]] && OPP_CHANGES_DOCKERFILE=1 && continue
   [[ $sf == operators* ]] && OPP_CHANGES_IN_OPERATORS_DIR=1
   # [[ $sf == upstream-community-operators* ]] && OPP_CHANGES_STREAM_UPSTREAM=1
