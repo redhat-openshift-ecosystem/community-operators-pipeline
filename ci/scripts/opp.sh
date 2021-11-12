b9af62951038fe3d406c3518f3362073a1d37c87
Martin Vala <mavala@redhat.com>
2021-11-12 14:09:49 +0300

    Test
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp.sh
+++ ci/scripts/opp.sh
@@ -402,7 +402,7 @@ function ExecParameters() {
     [[ $1 == orange* ]] && [[ $OPP_PROD -eq 1 ]] && [[ $OPP_SKIP_INDEX -eq 1 ]] && OPP_EXEC_USER="$OPP_EXEC_USER -e index_skip=true"
 
 
-    [ -n "$IIB_INPUT_REGISTRY_USER" ] && OPP_EXEC_USER="$OPP_EXEC_USER -e quay_arch_input_user=$IIB_INPUT_REGISTRY_USER -e quay_arch_input_host=$(echo $OPP_MIRROR_INDEX_MULTIARCH_BASE | cut -d '/' -f 1)"
+    [ -n "$IIB_INPUT_REGISTRY_USER" ] && OPP_EXEC_USER="$OPP_EXEC_USER -e quay_arch_input_user=\"$IIB_INPUT_REGISTRY_USER\" -e quay_arch_input_host=$(echo $OPP_MIRROR_INDEX_MULTIARCH_BASE | cut -d '/' -f 1)"
     [ -n "$IIB_INPUT_REGISTRY_TOKEN" ] && OPP_EXEC_USER_SECRETS="$OPP_EXEC_USER_SECRETS -e quay_arch_input_token=\"$IIB_INPUT_REGISTRY_TOKEN\""
 
     # If community and doing orange_<version>
