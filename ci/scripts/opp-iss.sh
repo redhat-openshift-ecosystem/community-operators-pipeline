90cb219d307376c9fce3216788144abdc1b4faef
Martin Vala <mavala@redhat.com>
2021-11-12 06:50:54 +0300

    Fix
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-iss.sh
+++ ci/scripts/opp-iss.sh
@@ -96,6 +96,9 @@ OPP_EXEC_USER="-e catalog_repo=$OPP_THIS_REPO_BASE/$OPP_THIS_REPO -e catalog_rep
 OPP_EXEC_USER="$OPP_EXEC_USER -e bundle_index_image_version=$OPP_PRODUCTION_INDEX_IMAGE_TAG -e sis_index_image_input=$OPP_PRODUCTION_INDEX_IMAGE:$OPP_PRODUCTION_INDEX_IMAGE_TAG -e sis_index_image_output=$OPP_PRODUCTION_INDEX_IMAGE:${2}${OPP_INDEX_POSTFIX} -e op_base_name=operators"
 OPP_EXEC_USER_SECRETS="-e quay_api_token=$REGISTRY_RELEASE_API_TOKEN"
 
+[ -n "$IIB_INPUT_REGISTRY_USER" ] && OPP_EXEC_USER="$OPP_EXEC_USER -e quay_arch_input_user=$IIB_INPUT_REGISTRY_USER -e quay_arch_input_host=$(echo $OPP_MIRROR_INDEX_MULTIARCH_BASE | cut -d '/' -f 1)"
+[ -n "$IIB_INPUT_REGISTRY_TOKEN" ] && OPP_EXEC_USER_SECRETS="$OPP_EXEC_USER_SECRETS -e quay_arch_input_token='$IIB_INPUT_REGISTRY_TOKEN'"
+
 if [ -f "$OP_INFO_FILE_LOCATION/op_info.yaml" ]; then OPP_EXEC_USER="$OPP_EXEC_USER -e operator_info_file=$OP_INFO_FILE_LOCATION/op_info.yaml"; fi
 
 OPP_EXEC_USER="$OPP_EXEC_USER -e automatic_cluster_version_label=true"
