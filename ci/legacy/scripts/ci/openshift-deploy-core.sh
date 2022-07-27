222cd40dde7aa2f876da2ef811cc16ef0bf0506c
J0zi <jbreza@redhat.com>
2022-07-27 13:32:40 +0200

    clean
    Signed-off-by: J0zi <jbreza@redhat.com>

--- ci/legacy/scripts/ci/openshift-deploy-core.sh
+++ ci/legacy/scripts/ci/openshift-deploy-core.sh
@@ -236,7 +236,6 @@ if [ ! -d "operators/$OP_NAME/$OP_VER/manifests" ]; then
 fi
 #Prepare temp index
 echo "Preparing temp index ..."
-
 [[ $TEST_MODE -ne 1 ]] && OP_TOKEN=$(cat /var/run/cred/op_token_quay_test)
 echo
 [[ $TEST_MODE -ne 1 ]] && curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
