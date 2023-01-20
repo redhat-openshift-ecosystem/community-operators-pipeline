aa42dd04ac5177f628e3c6c51e7d1d431517fe4d
J0zi <jbreza@redhat.com>
2023-01-20 11:05:06 +0100

    IIB input registry user and token to temp index
    Signed-off-by: J0zi <jbreza@redhat.com>

--- ci/scripts/opp-oprt.sh
+++ ci/scripts/opp-oprt.sh
@@ -60,6 +60,8 @@ echo "git pull --rebase upstream $OPP_OPRT_SRC_BRANCH"
 git pull --rebase upstream $OPP_OPRT_SRC_BRANCH || handleError
 echo "Repo rebased over branch OPP_OPRT_SRC_BRANCH - $OPP_OPRT_SRC_BRANCH"
 
+[ -n "$IIB_INPUT_REGISTRY_USER" ] && OPP_EXEC_USER="$OPP_EXEC_USER -e registry_redhat_io_user=$IIB_INPUT_REGISTRY_USER"
+[ -n "$IIB_INPUT_REGISTRY_TOKEN" ] && OPP_EXEC_USER_SECRETS="$OPP_EXEC_USER_SECRETS -e registry_redhat_io_token=$IIB_INPUT_REGISTRY_TOKEN"
 export OPP_ADDED_FILES=$(git diff --diff-filter=A upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_MODIFIED_FILES=$(git diff --diff-filter=M upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_REMOVED_FILES=$(git diff --diff-filter=D upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
