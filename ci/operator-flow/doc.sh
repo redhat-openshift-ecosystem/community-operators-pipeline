c4cda080773ded379a8af4aa894dcf142b3de0b8
Martin Vala <mavala@redhat.com>
2023-02-20 15:07:47 +0100

    Improved doc.sh
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/operator-flow/doc.sh
+++ ci/operator-flow/doc.sh
@@ -9,9 +9,15 @@ DOC_DIR="$(dirname $(dirname $(dirname $(readlink -m $0))))"/docs
 ORG=$(echo $REPO | cut -d '/' -f 1)
 PROJ=$(echo $REPO | cut -d '/' -f 2)
 
+rm -f results.json
 echo "$SCRIPT_DIR/measure-operator-flow.py -l $NPRS -o results.json -r $REPO -a $ADMINS"
 $SCRIPT_DIR/measure-operator-flow.py -l $NPRS -o results.json -r $REPO -a $ADMINS || true
-[ -f results.json ] || { curl https://${ORG}.github.io/PROJ/images/stats/results.json -O || cp $DOC_DIR/images/stats/results.json .; }
+if [ ! -f results.json ];then
+  status=$(curl -s -w "%{http_code}\\n" https://${ORG}.github.io/${PROJ}/images/stats/results.json -s -o /dev/null)
+  echo "$status"
+  [ "$status" == "200" ] && curl https://${ORG}.github.io/${PROJ}/images/stats/results.json -O
+fi
+[ -f results.json ] || cp $DOC_DIR/images/stats/results.json .
 $SCRIPT_DIR/ana.py
 [ -d $DOC_DIR/images/stats ] || mkdir -p $DOC_DIR/images/stats
 mv *.pdf *.png results.json $DOC_DIR/images/stats/
