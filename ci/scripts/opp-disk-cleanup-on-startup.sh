379209a23eda2a2de5f6f969ee6dffd323463f1d
Maurizio Porrato <mporrato@redhat.com>
2023-06-15 11:57:51 +0100

    Free up more space on the GH worker
    Signed-off-by: Maurizio Porrato <mporrato@redhat.com>

--- ci/scripts/opp-disk-cleanup-on-startup.sh
+++ ci/scripts/opp-disk-cleanup-on-startup.sh
@@ -33,11 +33,14 @@ echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 df -h
 echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get remove --purge -y '^ghc-8.*'
+sudo apt-get remove --purge -y '^dotnet-.*'
+sudo apt-get remove --purge -y '^aspnetcore-runtime-.*'
+sudo apt-get remove --purge -y '^llvm-.*'
+sudo apt-get remove --purge -y '^temurin-.*'
+sudo apt-get remove --purge -y '^r-base-.*'
+sudo apt-get remove --purge -y 'php.*'
+sudo apt-get remove --purge -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel mono-llvm-tools monodoc-manual mono-utils msbuild microsoft-edge-stable
 sudo apt-get autoremove -y
 sudo apt-get clean
 df -h
