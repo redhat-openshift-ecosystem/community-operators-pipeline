d9b48c571fd4cc652789f074d054ce944e798ff6
Tomas Man <tman@redhat.com>
2025-12-03 12:09:46 +0100

    Fix callback deprecation in GitHub workflows
    Signed-off-by: Tomas Man <tman@redhat.com>

--- ci/scripts/opp-oprt-cosmetics.sh
+++ ci/scripts/opp-oprt-cosmetics.sh
@@ -6,5 +6,5 @@ ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH -i localhost,
 
 for CSV_TO_TEST in $OPP_MODIFIED_CSVS; do
     CSV_PATH_TO_TEST="https://raw.githubusercontent.com/$OPP_MODIFIED_REPO_BRANCH/$CSV_TO_TEST"
-    ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true --tags cosmetics -e run_bundle_test=true -e run_remove_catalog_repo=false -vv -e csv_external_file_path=$CSV_PATH_TO_TEST -e run_prepare_catalog_repo_upstream=false -e dc_stream_name=operators
+    ANSIBLE_CALLBACK_RESULT_FORMAT=yaml ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true --tags cosmetics -e run_bundle_test=true -e run_remove_catalog_repo=false -vv -e csv_external_file_path=$CSV_PATH_TO_TEST -e run_prepare_catalog_repo_upstream=false -e dc_stream_name=operators
 done
